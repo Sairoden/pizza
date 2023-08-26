19c6d1ae36799000083107c50bdc7588ceb8d55d
Sairoden <odingandarosa@gmail.com>
2023-08-26 16:19:49 +0800

    Setup file structures

---
+++ src/services/apiGeocoding.js
@@ -0,0 +1,16 @@
+async function getAdress({ latitude, longitude }) {
+  try {
+    const res = await fetch(
+      `https://api.bigdatacloud.net/data/reverse-geocode-client?latitude=${latitude}&longitude=${longitude}`
+    );
+
+    if (!res.ok) throw Error("Failed getting address");
+
+    const data = await res.json();
+    return data;
+  } catch (err) {
+    console.log(err.message);
+  }
+}
+
+export default getAdress;
---
+++ src/services/index.js
@@ -0,0 +1,4 @@
+import getAdress from "./apiGeocoding/getAdress";
+import { getMenu, getOrder, createOrder, updateOrder } from "./apiRestaurant";
+
+export { getAdress, getMenu, getOrder, createOrder, updateOrder };
