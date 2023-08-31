ce01b32dbd9d14451faea1a542a45f18a74de18d
Sairoden <odingandarosa@gmail.com>
2023-08-31 16:34:16 +0800

    Reuse button style

--- src/ui/index.js
+++ src/ui/index.js
@@ -3,5 +3,7 @@ import Home from "./Home";
 import Header from "./Header";
 import AppLayout from "./AppLayout";
 import Loader from "./Loader";
+import Button from "./Button";
+import LinkButton from "./LinkButton";
 
-export { Error, Home, Header, AppLayout, Loader };
+export { Error, Home, Header, AppLayout, Loader, Button, LinkButton };
